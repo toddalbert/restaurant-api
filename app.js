3843915ed4b84c70441b6a98ca3a6eaf17c9c8a9
Todd Albert <todd@myphoto.com>
2020-06-24 12:06:45 -0400

    starting to build my API services

---
+++ app.js
@@ -0,0 +1,22 @@
+// import express server:
+const express = require('express')
+// install body-parser library to handle data in post requests:
+const bodyParser = require('body-parser')
+// import our "menu" function:
+const { getMenu, addToMenu } = require('./src/menu')
+// sets up our express server: calls it "app"
+const app = express()
+const port = 3005
+
+// use body-parser for our server:
+app.use(bodyParser.json())
+
+// GET ROUTES
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/menu', getMenu) // returns the whole menu
+app.get('/menu/:category', getMenu) // returns a menu section
+
+// POST ROUTES
+app.post('/menu/add', addToMenu) // adds a new menu item
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
---
+++ src/menu.js
@@ -0,0 +1,67 @@
+let ourMenu = {
+  appetizers: [{
+    name: 'Egg Rolls',
+    price: 3.99,
+    vegetarian: false
+  }, {
+    name: 'Spring Roll',
+    price: 1.99,
+    vegetarian: true
+  }, {
+    name: 'Spare Ribs',
+    price: 5.99,
+    vegetarian: false
+  }],
+  soups: [{
+    name: 'Egg Drop',
+    price: 3.99,
+    vegetarian: false
+  }, {
+    name: 'Wonton',
+    price: 3.99,
+    vegetarian: false
+  }, {
+    name: 'Vegetable',
+    price: 3.99,
+    vegetarian: true
+  }],
+  entrees: [{
+    name: 'Kung Pao Chicken',
+    price: 12.99,
+    vegetarian: false
+  }, {
+    name: 'Buddha\'s Delight',
+    price: 11.99,
+    vegetarian: true
+  }, {
+    name: 'General Tso Chicken',
+    price: 12.99,
+    vegetarian: false
+  }, {
+    name: 'Sesame Beef',
+    price: 14.99,
+    vegetarian: false
+  }]
+}
+
+exports.getMenu = (request, reply) => {
+  if (request.params && request.params.category) {
+    let { category } = request.params
+    if (ourMenu.hasOwnProperty(category)) {
+      let menuCategory = ourMenu[category] // use .filter
+      // let results = jobsArray.filter(job => job.city === 'Boca Raton')
+      reply.status(200).send(menuCategory)
+    } else {
+      reply.status(401).send('Error: Menu category not found.')
+    }
+  }
+  reply
+    .status(200)
+    .send(ourMenu)
+}
+
+exports.addToMenu = (request, reply) => {
+  // do some stuff
+  console.log(request.body)
+  reply.status(200).send('Menu item added. ')
+}
